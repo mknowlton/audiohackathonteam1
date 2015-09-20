fd127876b130ecedbfe6f3536f6d806a2bbea434
shinson <sonia.hinson@gmail.com>
2015-09-20 11:53:15 -0400

    added categories images and css styling(still working on this)

--- app/scripts/services/CategoriesService.js
+++ app/scripts/services/CategoriesService.js
@@ -6,7 +6,7 @@ angular.module('queueCastApp')
 //Names are categories come from Audiosearch except Motivation and Feel, text is what shows up in Categories  View
 //Change names as needed, name will pull the playlist
 function CategoriesService() {
-	this.categoreies = [{'name': 'Education', 'text': 'Learn', 'image':'http://uxrepo.com/static/icon-sets/elusive/svg/headphones.svg'}, {'name': 'Comedy', 'text': 'Laugh', 'image':'http://uxrepo.com/static/icon-sets/elusive/svg/headphones.svg'}, {'name':'Technology', 'text': 'See the Future', 'image':'http://uxrepo.com/static/icon-sets/elusive/svg/headphones.svg'},{'name':'Motivation', 'text': 'Get Inspired', 'image':'http://uxrepo.com/static/icon-sets/elusive/svg/headphones.svg'}, {'name': 'Emotion', 'text':'Feel', 'image':'http://uxrepo.com/static/icon-sets/elusive/svg/headphones.svg'}];
+	this.categoreies = [{'name': 'Education', 'text': 'Learn', 'image':'images/learn.svg'}, {'name': 'Comedy', 'text': 'Laugh', 'image':'images/laugh.svg'}, {'name':'Technology', 'text': 'See the Future', 'image':'images/seeTheFuture.svg'},{'name':'Motivation', 'text': 'Get Inspired', 'image':'images/getInspired.svg'}, {'name': 'Emotion', 'text':'Feel', 'image':'images/feel.svg'}];
 
 	this.getCategories = function () {
 		return this.categoreies;
