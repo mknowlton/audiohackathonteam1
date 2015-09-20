ac5d5de7e6dc8504bad0ae1952a29ffef9b8dd8b
Jackie Benowitz <jackiebenowitz@Jackies-MacBook-Pro.local>
2015-09-20 11:56:07 -0400

    starting api factory

---
+++ app/scripts/api/EpisodeAPI.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('queueCastApp')
+  .factory('EpisodeAPI', EpisodeAPI);
+
+ function EpisodeAPI () {
+ 	var factory = {};
+
+ 	factory.getEpisodesByCategory = function(category) {
+ 		
+ 	};
+
+ 	return factory;
+ }
